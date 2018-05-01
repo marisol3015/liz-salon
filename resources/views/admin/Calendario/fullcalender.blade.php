6237a81c646a1b7fce7e8a1f7bc078465fab5347
marisol <mary.3015@homtail.com>
2018-05-01 13:20:25 -0500

    cambio en el view fullcalendar

--- resources/views/admin/Calendario/fullcalender.blade.php
+++ resources/views/admin/Calendario/fullcalender.blade.php
@@ -2,13 +2,9 @@
     <head>
 <!-- jQuery -->
 <script src='/fullcalendar-3.9.0/lib/jquery.min.js'></script>
-
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="/fullcalendar-3.9.0/fullcalendar.min.css"/>
-
 <script src="/fullcalendar-3.9.0/fullcalendar.min.js"></script>
-
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="/fullcalendar-3.9.0/fullcalendar.min.css"/>
 
 </head>
 
