d39236ca29cf7eb0719091a9704951363a3e3e21
marisol <mary.3015@homtail.com>
2018-05-01 10:29:19 -0500

    migracion de eventos

---
+++ database/migrations/create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('documento')->unique();
+            $table->string('apellido');
+            $table->string('direccion');
+            $table->int('telefono');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
