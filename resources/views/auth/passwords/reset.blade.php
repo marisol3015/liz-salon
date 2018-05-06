e42a1a1e906d266a556e7eda9ff51da157a44f7d
marisol <mary.3015@homtail.com>
2018-05-05 22:30:59 -0500

    contraseña

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -12,6 +12,7 @@
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
+                        <input type="hidden" name="password" value="{{ $password }}">
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
