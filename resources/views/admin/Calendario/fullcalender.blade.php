24ed972c5f6915e821beee2bbfa5162b9e86b461
marisol <mary.3015@homtail.com>
2018-05-06 15:51:26 -0500

    prueba2

--- resources/views/admin/Calendario/fullcalender.blade.php
+++ resources/views/admin/Calendario/fullcalender.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
 <!-- jQuery -->
-<script src='fullcalendar-3.9.0/lib/jquery.min.js'></script>
+<script src='/fullcalendar-3.9.0/lib/jquery.min.js'></script>
 
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -25,10 +25,10 @@
     </div>
 </div>
 </body>
-<script src="fullcalendar-3.9.0/lib/moment.min.js"></script>
-<script src="fullcalendar-3.9.0/fullcalendar.min.js"></script>
+<script src="/fullcalendar-3.9.0/lib/moment.min.js"></script>
+<script src="/fullcalendar-3.9.0/fullcalendar.min.js"></script>
 {!! $calendar->script() !!}
-<script src="{{asset('fullcalendar-3.9.0/locale/es.js')}}"></script>
+<script src="{{asset('/fullcalendar-3.9.0/locale/es.js')}}"></script>
 </html>
 
 
